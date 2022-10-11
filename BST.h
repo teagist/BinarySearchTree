2a9fbb481ac9fee5e1bee9cc30e7d1a60d1c79d4
Houston Brown <80162431+teagist@users.noreply.github.com>
2022-10-10 23:19:04 -0500

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,84 @@
+//***************************************************************************
+//	Header File for Templated Binary Search Tree
+//	Programmed by: Houston Brown
+//	Last Compiled Date: 8/4/2022
+//***************************************************************************
+
+#ifndef _BST_H
+#define _BST_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+
+template<class T>
+class BST;
+
+
+
+template <class T>
+class BNode
+{
+    T data;
+    BNode* left;
+    BNode* right;
+
+    BNode(T newData): data(newData), left(NULL), right(NULL){};
+
+    friend class BST<T>;
+};
+
+
+
+template <class T>
+class BST
+{
+public:
+    BST(): root(NULL){};                 
+
+    void Insert(const T key) { return InsertHelper(key, root); }
+	bool Search(const T key);
+	void Delete(const T key);
+
+	void PrintTree() const;
+    void InOrder() const;
+    void PreOrder() const;
+    void PostOrder() const;
+
+    BNode<T>* GetMin(BNode<T>* subtree) const;
+
+    void PrintCursor() const;
+
+    BST(const BST& myBST);
+    ~BST();
+    BST& operator=(const BST& rightTree);
+
+private:
+    BNode<T>* root;
+    BNode<T>* cursor;
+
+    void InsertHelper(const T key, BNode<T>*& subtree);
+	BNode<T>* DeleteHelper(const T key, BNode<T>*& subtree);   
+	BNode<T>* GetParent(BNode<T>* subtree); 
+
+	// Printing in Level Order Helper Methods
+	int TreeHeight(BNode<T>* subtree) const;
+	void PrintLevel(int height) const;
+	void DispLevel(BNode<T>* subtree, int level, int displace) const;
+	
+	// Printing in Other Order Helper Methods	
+    void PreOrderHelper(const BNode<T>* subtree) const; 
+    void InOrderHelper(const BNode<T>* subtree) const;
+    void PostOrderHelper(const BNode<T>* subtree) const;
+
+    void CopyHelper(BNode<T>*& root, BNode<T>* myRoot);
+    void DestroyHelper(BNode<T>*& Tree);
+};
+
+
+#endif
